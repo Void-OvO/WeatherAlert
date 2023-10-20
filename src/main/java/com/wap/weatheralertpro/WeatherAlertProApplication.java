e9b742d0f7e0ab4c7e4fe96184256bf2fc5ff532
양현성 <125037243+Void-OvO@users.noreply.github.com>
2023-10-20 20:27:22 +0900

    양현성 : 프로젝트 초기 세팅

---
+++ src/main/java/com/wap/weatheralertpro/WeatherAlertProApplication.java
@@ -0,0 +1,13 @@
+package com.wap.weatheralertpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAlertProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherAlertProApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/wap/weatheralertpro/WeatherAlertProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wap.weatheralertpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAlertProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
