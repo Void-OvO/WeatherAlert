c7f879d3d6862612a13b7f5aa502ee87fb22d20f
양현성 <125037243+Void-OvO@users.noreply.github.com>
2023-10-25 14:14:14 +0900

    양현성 : 프로젝트 초기 세팅

---
+++ src/main/java/com/wa/weatheralert/WeatherAlertApplication.java
@@ -0,0 +1,13 @@
+package com.wa.weatheralert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAlertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherAlertApplication.class, args);
+    }
+
+}
